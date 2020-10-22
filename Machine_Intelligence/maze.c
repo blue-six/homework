e98c67af2ce0f2a17eb1a6dee3d92e25e439f6c6
blue-six <63455694+blue-six@users.noreply.github.com>
2020-10-22 21:26:38 +0800

    加入迷宫注释

--- Machine_Intelligence/maze.c
+++ Machine_Intelligence/maze.c
@@ -6,11 +6,11 @@
 
 typedef struct path
 {
-    unsigned int x : 8, y : 8, slope : 8;
-    unsigned int deep : 9, value : 9;
-    unsigned int weight : 9;
-    unsigned int closed : 1;
-    struct path *next, *parent;
+    unsigned int x : 8, y : 8, slope : 8; //坐标值x，y，以及目标偏离程度slope
+    unsigned int deep : 9, value : 9;     //深度，估值
+    unsigned int weight : 9;              //权值
+    unsigned int closed : 1;              //节点关闭标志
+    struct path *next, *parent;           //下一个节点，父节点
 } P;
 
 void createMaze();                         //创建随机迷宫
@@ -23,9 +23,9 @@ int get_value(int x, int y);               //取得h值
 P *get(P *head);                           //取得权值最小的节点
 void add(P *head, P *n);                   //添加节点
 P *new_p(int x, int y, int deep);          //创建新节点
-int refresh(P *head, P *n);
-int main_1(void);
-int main_2(void);
+int refresh(P *head, P *n);                //更新节点在open_list中的位置
+int main_1(void);                          //原始主函数
+int main_2(void);                          //魔改后用于测验的主函数
 
 int startI = 1,
     startJ = 1; // 入口
@@ -105,7 +105,7 @@ int main_1(void)
     return 0;
 }
 
-int main_2(void)
+int main_2(void) //连续生成固定大小的随机迷宫并求解，同时展示扩展的节点以及相关数据统计。
 {
     int i, j;
     row = 20;
@@ -239,7 +239,7 @@ void createFreeMaze()
     maze[row - 2][col - 2] = 0; //出口为通道
 }
 
-int visit_aimless_dfs(int row2, int col2)
+int visit_aimless_dfs(int row2, int col2) //盲目深度优先搜索。
 {
     if (maze[row2][col2] != 0)
         return 0;
@@ -247,53 +247,58 @@ int visit_aimless_dfs(int row2, int col2)
     if (row2 == row - 2 && col2 == row - 2)
         return 1;
     if (visit_aimless_dfs(row2, col2 + 1) || visit_aimless_dfs(row2 + 1, col2) ||
-        visit_aimless_dfs(row2 - 1, col2) || visit_aimless_dfs(row2, col2 - 1))
+        visit_aimless_dfs(row2 - 1, col2) || visit_aimless_dfs(row2, col2 - 1)) //有一个可解即为可解。
         return 1;
     maze[row2][col2] = 0;
     return 0;
 }
 
-int visit_A_star(int row2, int col2)
+int visit_A_star(int row2, int col2) //通过A*算法寻找迷宫出路
 {
-    P *map[row][col];
-    memset(map, 0, row * col * sizeof(P *));
+    P *map[row][col];                        //存储节点信息的数组
+    memset(map, 0, row * col * sizeof(P *)); //将数组内容初始化为0
     char x, y, flg = 0;
-    int deep;
+    int deep; //当前遍历到的节点的相关信息
     P *open_list = (P *)malloc(sizeof(P)), *p, *q;
     map[row2][col2] = p = open_list->next = (P *)malloc(sizeof(P));
     p->x = row2, p->y = col2, p->deep = 0, p->next = p->parent = NULL;
     p->slope = (p->y / p->x) - abs(col - col2) / abs(row - row2);
-    p->value = get_value(p->x, p->y), p->weight = p->deep + p->value;
-    while (open_list->next != NULL)
+    p->value = get_value(p->x, p->y), p->weight = p->deep + p->value; //初始化open_list
+    while (open_list->next != NULL)                                   //通过判断open_list是否为空来跳出循环
     {
-        p = get(open_list);
-        p->closed = 1;
-        if (p->x == row - 2 && p->y == col - 2 && (++flg))
+        p = get(open_list);                                //从open_list中移出权值最小的节点
+        p->closed = 1;                                     //将此节点标记为关闭
+        if (p->x == row - 2 && p->y == col - 2 && (++flg)) //如果找到出口即将flg置1并跳出循环
             break;
-        x = p->x, y = p->y, deep = p->deep;
-        if (maze[x + 1][(short)y] != 2)
+        x = p->x, y = p->y, deep = p->deep; //得到节点相关信息
+        /* 
+        进入扩展阶段
+        依次向下、右、上、左遍历节点并加入open_list当中，
+        同时将对应的节点记录到存储节点信息数组的对应位置当中。
+         */
+        if (maze[x + 1][(short)y] != 2) //如果此节点不是墙，即可进行添加操作
         {
-            if (map[x + 1][(short)y] == 0)
+            if (map[x + 1][(short)y] == 0) //如果当前节点的信息未记录到数组中，就创建新节点并记录
             {
                 q = map[x + 1][(short)y] = new_p(x + 1, y, deep + 1);
-                add(open_list, q);
-                q->parent = map[(short)x][(short)y];
+                add(open_list, q);                   //将节点加入到open_list当中
+                q->parent = map[(short)x][(short)y]; //将此节点的父节点设置为当前节点
             }
-            else
+            else //如果节点已存在
             {
-                q = map[x + 1][(short)y];
-                if (q->deep > deep + 1)
+                q = map[x + 1][(short)y]; //取得节点信息
+                if (q->deep > deep + 1)   //如果当前路径比已有路径短，进行节点更新
                 {
                     //printf("start  %d   ", q->deep);
                     //printf("end  %d\n", deep + 1);
-                    q->deep = deep + 1;
-                    q->weight = q->deep + q->value;
-                    q->closed == 0 && refresh(open_list, q);
-                    q->parent = map[(short)x][(short)y];
+                    q->deep = deep + 1;                      //更新节点深度
+                    q->weight = q->deep + q->value;          //更新节点权值
+                    q->closed == 0 && refresh(open_list, q); //如果节点未关闭，就进行更新此节点在open_list中的位置
+                    q->parent = map[(short)x][(short)y];     //将此节点的父节点设置为当前节点
                 }
             }
         }
-        if (maze[(short)x][y + 1] != 2)
+        if (maze[(short)x][y + 1] != 2) //逻辑与上面相同
         {
             if (map[(short)x][y + 1] == 0)
             {
@@ -315,7 +320,7 @@ int visit_A_star(int row2, int col2)
                 }
             }
         }
-        if (maze[x - 1][(short)y] != 2)
+        if (maze[x - 1][(short)y] != 2) //逻辑与上面相同
         {
             if (map[x - 1][(short)y] == 0)
             {
@@ -337,7 +342,7 @@ int visit_A_star(int row2, int col2)
                 }
             }
         }
-        if (maze[(short)x][y - 1] != 2)
+        if (maze[(short)x][y - 1] != 2) //逻辑与上面相同
         {
             if (map[(short)x][y - 1] == 0)
             {
@@ -360,81 +365,81 @@ int visit_A_star(int row2, int col2)
             }
         }
     }
-    if (flg)
+    if (flg) //如果flg为1，说明找到出口
     {
-        for (q = p; q != NULL; q = q->parent)
+        for (q = p; q != NULL; q = q->parent) //在迷宫图中记录找到的路径
             maze[q->x][q->y] = 1;
     }
-    int n = 0, m = 0;
-    for (int i = 0; i < row; i++)
+    int n = 0, m = 0;             //统计数据用变量
+    for (int i = 0; i < row; i++) //对整个节点信息存储数组进行遍历
     {
         for (int j = 0; j < col; j++)
         {
-            if (map[i][j] != 0)
+            if (map[i][j] != 0) //如果存储不为0，说明存有节点信息
             {
-                ++m;
-                if (map[i][j]->closed && ++n)
-                    maze[i][j] = maze[i][j] == 0 ? 6 : maze[i][j];
+                ++m;                                               //生成节点数加1
+                if (map[i][j]->closed && ++n)                      //如果节点已关闭，扩展节点数加1
+                    maze[i][j] = maze[i][j] == 0 ? 6 : maze[i][j]; //如果迷宫此处没有其他记录，标记为已扩展
                 else
-                    maze[i][j] = maze[i][j] == 0 ? 5 : maze[i][j];
-                free(map[i][j]);
+                    maze[i][j] = maze[i][j] == 0 ? 5 : maze[i][j]; //如果没有扩展，标记为未扩展
+                free(map[i][j]);                                   //释放此节点内存
             }
         }
     }
-    printf("未扩展结点:%d  找到的节点/总结点:%d/%d", m - n, m, (row - 2) * (col - 2));
+    printf("未扩展结点:%d  找到的节点/总结点:%d/%d", m - n, m, (row - 2) * (col - 2)); //输出相关信息
     if (flg)
-        return 1;
-    return 0;
+        return 1; //成功退出
+    return 0;     //失败退出
 }
 
-void add(P *head, P *n)
+void add(P *head, P *n) //向open_list中加如节点
 {
     P *p = head, *q = p;
-    if (head->next == NULL)
+    if (head->next == NULL) //头节点之后为空的相关判断
     {
-        head->next = n;
+        head->next = n; //之后为空则直接链接返回
         return;
     }
     else
-        q = q->next;
-    char flg = 1;
-    while (q != NULL && q->next != NULL)
+        q = q->next;                     //否则当前指针向后移动
+    char flg = 1;                        //加入失败标志
+    while (q != NULL && q->next != NULL) //变历到末尾就退出
     {
-        if (q->weight < n->weight ||
-            (q->weight == n->weight &&
-             (q->value < n->value ||
-              (q->value == n->value &&
-               q->slope < n->slope))))
+        if (q->weight < n->weight ||   //排序的判断条件，权值小的放在前方
+            (q->weight == n->weight && //如果权值相等
+             (q->value < n->value ||   //估值小的放在前面
+              (q->value == n->value && //如果估值相等
+               q->slope < n->slope)))) //对目标偏离较小的放在前面
             p = p->next, q = q->next;
-        else
+        else //满足条件后进行插入
         {
             n->next = q, p->next = n;
-            flg = 0;
-            break;
+            flg = 0; //加入成功flg置0
+            break;   //插入完成跳出循环
         }
     }
-    if (flg)
+    if (flg) //如果加入失败，直接链接到末尾
         q->next = n;
 }
 
-int refresh(P *head, P *n)
+int refresh(P *head, P *n) //对单一节点重新排序
 {
     P *p = head, *q = head->next, *now = NULL;
-    while (q != NULL && q->next != NULL)
+    while (q != NULL && q->next != NULL) //遍历到末尾就退出
     {
-        if ((q->weight == n->weight &&
+        if ((q->weight == n->weight && //重新排序的规则，与add()相同
              (q->value > n->value ||
               (q->value == n->value &&
                q->slope > n->slope))) ||
             q->weight > n->weight)
-            now = p;
+            now = p; //找到目标位置并记录
         p = p->next;
-        if (q->next != n)
+        if (q->next != n) //找到节点所在位置
             q = q->next;
         else
             break;
     }
-    if (q->next && now != NULL)
+    if (q->next && now != NULL) //进行重排链接
     {
         head = now->next;
         now->next = n;
@@ -444,10 +449,10 @@ int refresh(P *head, P *n)
     return 1;
 }
 
-P *get(P *head)
+P *get(P *head) //得到open_list中的最小节点并删除
 {
     P *p = head->next;
-    if (head->next != NULL)
+    if (head->next != NULL) //由于已排好顺序，故取头节点之后的一个节点即可
     {
         head->next = p->next;
         p->next = NULL;
@@ -456,21 +461,21 @@ P *get(P *head)
     return NULL;
 }
 
-P *new_p(int x, int y, int deep)
+P *new_p(int x, int y, int deep) //创建新节点
 {
     int n;
-    P *p = (P *)malloc(sizeof(P));
+    P *p = (P *)malloc(sizeof(P)); //申请内存空间
     p->deep = deep, p->closed = 0;
     p->x = x, p->y = y;
     n = row - 2 - x;
     p->slope = abs((col - 1 / row - 1) - abs(n == 0 ? INT_MAX : (col - 2 - y) / n));
     p->value = get_value(p->x, p->y);
     p->weight = p->deep + p->value;
-    p->next = p->parent = NULL;
+    p->next = p->parent = NULL; //进行相关初始化
     return p;
 }
 
-int get_value(int x, int y)
+int get_value(int x, int y) //获得传入位置的路径估值
 {
     return abs(row - 2 - x) + abs(col - 2 - y);
 }
